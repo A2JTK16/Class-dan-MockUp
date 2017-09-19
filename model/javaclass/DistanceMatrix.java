7758211e4fcbc85d880b0bfcc24b9ec25cc1b7fb
Auliya Aqma Dinillah <auliyaaqma@gmail.com>
2017-09-20 05:32:29 +0700

    update distance matrix class

--- model/javaclass/DistanceMatrix.java
+++ model/javaclass/DistanceMatrix.java
@@ -3,36 +3,42 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package belajarjava;
+package travlendara2;
 
 /**
  *
  * @author ASUS
  */
 public class DistanceMatrix {
-    
-  /**
-   * Origin addresses as returned by the API from your original request. These are formatted by the
-   * geocoder and localized according to the language parameter passed with the request.
-   */
-  public final String[] originAddresses;
+    //atribute
+    private String nama_kota;
+    private String nama_kota2;
 
-  /**
-   * Destination addresses as returned by the API from your original request. As with {@link
-   * #originAddresses}, these are localized if appropriate.
-   */
-  public final String[] destinationAddresses;
+    /**
+     * @return the nama_kota
+     */
+    public String getNama_kota() {
+        return nama_kota;
+    }
 
-  /**
-   * An array of elements, each of which in turn contains a status, duration, and distance element.
-   */
-  public final DistanceMatrixRow[] rows;
+    /**
+     * @param nama_kota the nama_kota to set
+     */
+    public void setNama_kota(String nama_kota) {
+        this.nama_kota = nama_kota;
+    }
 
-  public DistanceMatrix(
-      String[] originAddresses, String[] destinationAddresses, DistanceMatrixRow[] rows) {
-    this.originAddresses = originAddresses;
-    this.destinationAddresses = destinationAddresses;
-    this.rows = rows;
-  }
+    /**
+     * @return the nama_kota2
+     */
+    public String getNama_kota2() {
+        return nama_kota2;
+    }
 
+    /**
+     * @param nama_kota2 the nama_kota2 to set
+     */
+    public void setNama_kota2(String nama_kota2) {
+        this.nama_kota2 = nama_kota2;
+    }
 }
